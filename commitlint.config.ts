b1c6cfe7be6e71cb85cd99c205ce6e5d480fa5d9
Adryel Rocha <adryelrbrz+github@gmail.com>
2026-06-18 18:50:34 +0100

    chore(tools): enforce valid scopes in commits (#8)

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,14 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-empty': [2, 'never'],
+        'scope-enum': [
+            2,
+            'always',
+            ['web', 'api', 'deps', 'arch', 'infra', 'tools', 'actions', 'product', 'templates'],
+        ],
+    },
 };
 
 export default config;
