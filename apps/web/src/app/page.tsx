29cdb34f96a813cbe43a4e0186a44e2ef540901b
Adryel Bezerra <adryelrbrz+github@gmail.com>
2026-07-30 17:39:29 +0100

    feat(web): add basic seo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,16 @@
-import { ThemeToggle } from '@checkmate/ui/internal/theme';
+import { Metadata } from 'next';
+
+import { Header } from '@/components/header';
+
+export const metadata: Metadata = {
+    title: 'Checkmate | Automate Your Business',
+    description: 'AI Solutions to Boost Your Business.',
+};
 
 export default function Page() {
     return (
         <main>
-            <ThemeToggle />
+            <Header />
         </main>
     );
 }
